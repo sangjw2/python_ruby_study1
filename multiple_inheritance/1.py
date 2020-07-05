c6115f96cec66a59de068b805bd78512f0331901
sangjw2 <sangjw2@gmail.com>
2020-07-05 19:58:36 +0900

    on Desktop

--- multiple_inheritance/1.py
+++ multiple_inheritance/1.py
@@ -1,12 +1,22 @@
 class C1():
-  def c1_m(self):
-    print('c1_m')
+	def c1_m(self):
+		print("c1_m")
+	def m(self):
+		print("C1 m")
+
 class C2():
-  def c2_m(self):
-    print('c2_m')
+	def c2_m(self):
+		print("c2_m")
+	def m(self):
+		print("C1 m")
+
 class C3(C1,C2):
-  pass
+	def m(self):
+		print("C3 m")
+	pass
 
 c = C3()
 c.c1_m()
 c.c2_m()
+c.m()
+print(C3.__mro__)
