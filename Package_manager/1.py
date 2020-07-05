e8ecc2f579eeb1617d3f81edfaebf7688dafb42b
sangjw2 <sangjw2@gmail.com>
2020-07-05 22:17:32 +0900

    web parsing..

--- Package_manager/1.py
+++ Package_manager/1.py
@@ -1,7 +1,13 @@
 import requests
-r = requests.get("https://qpi.github.com/iser", auth=("user", "pass"))
-r.status_code
-r.headers['content-type']
-r.encoding
-r.text
-r.json
+from bs4 import BeautifulSoup
+r = requests.get("https://codingeverybody.github.io/scraping_sample/1.html")
+soup = BeautifulSoup(r.text, 'html.parser')
+print('Title : '+soup.title.string)
+articles = soup.findAll("div", {"class": "em"})
+print('Article : '+articles[0].text)
+print("=================================")
+r2 = requests.get("https://codingeverybody.github.io/scraping_sample/2.html")
+soup2 = BeautifulSoup(r2.text, 'html.parser')
+print('Title : '+soup2.title.string)
+articles2 = soup2.findAll("div", {"class": "strong"})
+print("Article : "+articles2[0].text)
